3d2c0cdd700e5a54cd5056966c43704de7efc3ae
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-10-19 16:42:16 -0300

    feet: retuning  a friendly error message

---
+++ src/main/java/com/yanvelasco/ecommerce/domain/category/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.yanvelasco.ecommerce.domain.category.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> notValidException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exception(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+}

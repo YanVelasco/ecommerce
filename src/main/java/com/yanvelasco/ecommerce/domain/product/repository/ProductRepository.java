0fcdcb883b469ed7b3ab5b3f4a56fb38ac878e5c
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-11-01 11:36:49 -0300

    refactor: change ProductRepository ID type from Long to UUID

--- src/main/java/com/yanvelasco/ecommerce/domain/product/repository/ProductRepository.java
+++ src/main/java/com/yanvelasco/ecommerce/domain/product/repository/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.yanvelasco.ecommerce.domain.product.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.yanvelasco.ecommerce.domain.product.entity.ProductEntity;
 
 @Repository
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
 }

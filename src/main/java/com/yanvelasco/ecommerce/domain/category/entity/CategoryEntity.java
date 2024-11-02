b19d74cbeb31e3c660855ddb9139bcae33cd3aef
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-11-02 16:38:30 -0300

    feat: add products relationship to CategoryEntity with OneToMany mapping

--- src/main/java/com/yanvelasco/ecommerce/domain/category/entity/CategoryEntity.java
+++ src/main/java/com/yanvelasco/ecommerce/domain/category/entity/CategoryEntity.java
@@ -1,11 +1,16 @@
 package com.yanvelasco.ecommerce.domain.category.entity;
 
+import java.util.List;
 import java.util.UUID;
 
+import com.yanvelasco.ecommerce.domain.product.entity.ProductEntity;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -31,4 +36,7 @@ public class CategoryEntity {
     @NotBlank(message = "Category name is required")
     @Size(min = 3, max = 50, message = "Category name must be between 3 and 50 characters")
     private String name;
+
+    @OneToMany(mappedBy = "category", cascade=CascadeType.ALL)
+    private List<ProductEntity> products;
 }
